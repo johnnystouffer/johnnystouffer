8e4fce6c21362e3728be0efed9fe9b2b89841a3a
johnnystouffer <stouff17@msu.edu>
2025-09-20 16:51:34 -0400

    need to commit for safety check

---
+++ js-personal-website/src/app/topbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function Topbar() {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  return (
+<nav className="flex items-center justify-between shadow-md">
+  {/* Left */}
+  <div className="flex-1 flex">
+    <h1 className="text-4xl font-extrabold">John Stouffer</h1>
+  </div>
+
+  {/* Center */}
+  <ul className="flex gap-6 text-lg">
+    <li className="p-4 cursor-pointer hover:underline">Home</li>
+    <li className="p-4 cursor-pointer hover:underline">Experience</li>
+    <li className="p-4 cursor-pointer hover:underline">Projects</li>
+    <li className="p-4 cursor-pointer hover:underline">Other</li>
+    <li className="p-4 cursor-pointer hover:underline">Contact</li>
+  </ul>
+
+  {/* Right */}
+  <div className="flex-1 flex justify-end items-center gap-4">
+    <button
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      className="p-3 rounded-full cursor-pointer"
+      aria-label="Toggle theme"
+    >
+      {!mounted ? null : resolvedTheme === "dark"
+        ? <i className="las la-sun text-3xl" />
+        : <i className="las la-moon text-3xl" />}
+    </button>
+  </div>
+</nav>
+  );
+}
